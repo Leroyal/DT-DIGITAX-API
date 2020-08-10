ec28e7b95cf3699ad4057e5f48407b35ff6ce99e
Jayanta-Redapple <52992480+Jayanta-Redapple@users.noreply.github.com>
2020-08-10 21:26:10 +0530

    Update UserController.java

--- src/main/java/com/digitax/controller/UserController.java
+++ src/main/java/com/digitax/controller/UserController.java
@@ -7,13 +7,9 @@ import com.digitax.security.jwt.UserSession;
 import com.digitax.constants.ResponseConstants;
 import com.digitax.model.User;
 import com.digitax.model.UserProfile;
-import com.digitax.payload.ApiRes;
-import com.digitax.payload.response.JwtResponse;
 import com.digitax.payload.response.UserDetailsResponse;
 import com.digitax.repository.UserProfileRepository;
 import com.digitax.repository.UserRepository;
-import com.digitax.security.jwt.JwtUtils;
-import com.digitax.security.jwt.UserSession;
 import com.digitax.service.UserProfileService;
 
 import org.json.simple.JSONObject;
@@ -22,7 +18,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,9 +29,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.Optional;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
