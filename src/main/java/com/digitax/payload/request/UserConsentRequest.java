55e30e1d335e3415abb2876228cfaa1ea1440a8b
Jayanta-Redapple <52992480+Jayanta-Redapple@users.noreply.github.com>
2020-09-02 21:10:20 +0530

    Update UserConsentRequest.java

--- src/main/java/com/digitax/payload/request/UserConsentRequest.java
+++ src/main/java/com/digitax/payload/request/UserConsentRequest.java
@@ -1,10 +1,7 @@
 package com.digitax.payload.request;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-
-import com.digitax.model.TrueFalse;
 
 import lombok.Getter;
 import lombok.Setter;
